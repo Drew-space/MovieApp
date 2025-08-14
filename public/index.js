74c7ea12fecac33b22b910897b7a867d31bc4950
Drew <chukwukaenudeme@gmail.com>
2025-08-14 19:52:07 +0100

    before api

---
+++ public/index.js
@@ -0,0 +1,8 @@
+// import BG from "./hero-bg.png";
+// import Hero from "./hero.png";
+// import Logo from "./logo.png";
+// import nomovie from "./no-movie.png";
+// import StarIcon from "./star.svg";
+// import Search from "./search.svg";
+
+// export { BG, Hero, Logo, nomovie, StarIcon, Search };
